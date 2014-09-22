1da30baeb673aba2dc5c850f14fcef2bfcf9ab21
soultemptation <red_x@hotmail.com>
2014-09-22 11:01:25 +0200

    init

---
+++ src/main/java/ch/zhaw/swengineering/HelloApp.java
@@ -0,0 +1,16 @@
+package ch.zhaw.swengineering;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class HelloApp {
+    public static void main(String[] args) {
+	    ApplicationContext context = new AnnotationConfigApplicationContext(HelloApp.class);
+        HelloService helloService = context.getBean(HelloService.class);
+        System.out.println(helloService.sayHello());
+    }
+}
