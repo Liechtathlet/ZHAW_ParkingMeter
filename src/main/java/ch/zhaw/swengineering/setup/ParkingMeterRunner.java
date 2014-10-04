adef4981b6f5646d996c15fcead4e9235f3b0d6e
Dani <daniel.brun@gmx.net>
2014-10-04 09:29:19 +0200

    example logging

--- src/main/java/ch/zhaw/swengineering/setup/ParkingMeterRunner.java
+++ src/main/java/ch/zhaw/swengineering/setup/ParkingMeterRunner.java
@@ -1,5 +1,9 @@
 package ch.zhaw.swengineering.setup;
 
+import java.util.Arrays;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -17,22 +21,36 @@ import ch.zhaw.swengineering.LogAndXmlService;
 @EnableAutoConfiguration
 public class ParkingMeterRunner {
 
+	private static final Logger LOG = LogManager.getLogger(ParkingMeterRunner.class);
+	
 	/**
 	 * Entry-Method for the Java-Application
 	 * 
-	 * @param args The arguments.
+	 * @param args
+	 *            The arguments.
 	 */
 	public static void main(String[] args) {
+
+		LOG.info("Init application startup. Arguments: " + Arrays.toString(args));
 		
-		//TODO: Detect Parameter (GUI or Console), Default: Console
-		
-		//Start Spring
+		// Start Spring
 		ConfigurableApplicationContext context = SpringApplication.run(
 				ParkingMeterRunner.class, args);
+
+		if (args != null && args.length == 1) {
+			String versionParameter = args[0].trim().toLowerCase();
+
+			if (versionParameter.equals("gui")) {
+				LOG.info("Detected startup parameter for gui-version. Init gui...");
+				// TODO: Start GUI
+			}
+		}
 		
+		// TODO: if not gui: Start Console-Version
+
 		RunLogAndXmlService(context);
 	}
-	
+
 	/**
 	 * FIXME: Only for Test, delete after first implementation
 	 * 
@@ -46,6 +64,4 @@ public class ParkingMeterRunner {
 		System.out.println(service.run());
 	}
 
-	
-	
 }
