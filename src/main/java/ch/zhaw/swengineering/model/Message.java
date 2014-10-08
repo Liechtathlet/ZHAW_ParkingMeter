b901bd8d8c530acb8f638985d92941c22a477430
soultemptation <red_x@hotmail.com>
2014-10-08 22:39:02 +0200

    Adding missing model classes.

---
+++ src/main/java/ch/zhaw/swengineering/model/Message.java
@@ -0,0 +1,16 @@
+package ch.zhaw.swengineering.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlValue;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Message {
+
+	@XmlAttribute
+	public String key;
+
+	@XmlValue
+	public String value;
+}
---
+++ src/main/java/ch/zhaw/swengineering/model/Messages.java
@@ -0,0 +1,14 @@
+package ch.zhaw.swengineering.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "messages")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Messages {
+	@XmlElement(name = "message")
+	public List<Message> messages;
+}
