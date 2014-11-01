10f04252e916dc4cc73c4e42d19513bf7dee10b7
Roland <Roland@Ultrabook-Roli.home>
2014-11-01 14:43:05 +0100

    ParkingMeter GUI - Initialpush mit GUI-Komponenten

--- src/main/java/ch/zhaw/swengineering/view/gui/ParkingMeterPanel.java
+++ src/main/java/ch/zhaw/swengineering/view/gui/ParkingMeterPanel.java
@@ -3,13 +3,15 @@
  */
 package ch.zhaw.swengineering.view.gui;
 
-import java.awt.BasicStroke;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * @author Daniel Brun
@@ -22,16 +24,57 @@ public class ParkingMeterPanel extends JPanel {
 	 * Generates Serial Version UID.
 	 */
 	private static final long serialVersionUID = 1258929181774396319L;
-	
+
 	private static final Color BG = new Color(93, 92, 102);
 	private static final Color BG_INNER = new Color(26, 51, 115);
 	private static final Color BG_DISPLAY = new Color(95, 109, 96);
-	private static final Color BG_MARKER = new Color(200, 139, 50);
-	private static final Color BG_NUMBER_FIELD = new Color(200, 207, 215);
+	private static final Color BG_CANCEL = new Color(204, 0, 0);
+	private static final Color BG_OK = new Color(0, 153, 0);
+	private static final Color BG_SLUT = new Color(119, 136, 153);
+	private static final Color BG_TICKETFIELD = new Color(92, 172, 238);
+
+	// private static final Color BG_MARKER = new Color(200, 139, 50);
+	// private static final Color BG_NUMBER_FIELD = new Color(200, 207, 215);
 
 	private double factor = 3.0;
 	private int initialHeight;
 	private int initialWidth;
+
+	private static JPanel parkingMeterPane;
+	private static JPanel parkingBorderPane1;
+	private static JPanel feetPane;
+	private static JPanel parkingMeterMMI;
+	private static JPanel displayCoinsPane;
+	private static JPanel buttonSlotPane;
+	private static JPanel ticketPane;
+	private static JPanel displayPane;
+	private static JPanel coinsButtonPane;
+	private static JPanel buttonPane;
+	private static JPanel slotPane;
+	private static JPanel ticketfieldPane;
+
+	private static JTextArea display;
+
+	private static JButton coin1 = new JButton("Wert1");
+	private static JButton coin2 = new JButton("Wert2");
+	private static JButton coin3 = new JButton("Wert3");
+	private static JButton coin4 = new JButton("Wert4");
+
+	private static JButton button1 = new JButton("1");
+	private static JButton button2 = new JButton("2");
+	private static JButton button3 = new JButton("3");
+	private static JButton button4 = new JButton("4");
+	private static JButton button5 = new JButton("5");
+	private static JButton button6 = new JButton("6");
+	private static JButton button7 = new JButton("7");
+	private static JButton button8 = new JButton("8");
+	private static JButton button9 = new JButton("9");
+	private static JButton button0 = new JButton("0");
+	private static JButton buttonCancel = new JButton("C");
+	private static JButton buttonOk = new JButton("OK");
+
+	private static JTextArea coinSlut = new JTextArea("");
+	private static JTextArea ticketfield = new JTextArea();
 
 	public ParkingMeterPanel() {
 
@@ -39,159 +82,152 @@ public class ParkingMeterPanel extends JPanel {
 		initialWidth = 100;
 		setPreferredSize(new Dimension((int) (initialWidth * factor),
 				(int) (initialHeight * factor)));
-	}
 
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+		parkingMeterPane = new JPanel();
+		parkingBorderPane1 = new JPanel();
+		parkingMeterMMI = new JPanel();
+		feetPane = new JPanel();
+		displayCoinsPane = new JPanel();
+		buttonSlotPane = new JPanel();
+		ticketPane = new JPanel();
+		displayPane = new JPanel();
+		coinsButtonPane = new JPanel();
+		buttonPane = new JPanel();
+		slotPane = new JPanel();
+		ticketfieldPane = new JPanel();
 
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setStroke(new BasicStroke(1));
+		BorderLayout borderLayoutParkingMeterPane = new BorderLayout();
+		FlowLayout flowLayoutParkingBorderPane1 = new FlowLayout();
+		FlowLayout flowLayoutParkingMeterMMI = new FlowLayout(
+				FlowLayout.CENTER, 10, 10);
+		FlowLayout flowLayoutParkingDisplayCoinsPane = new FlowLayout(
+				FlowLayout.CENTER, 10, 10);
+		FlowLayout flowLayoutParkingButtonSlotPane = new FlowLayout(
+				FlowLayout.CENTER, 10, 20);
+		BorderLayout borderLayoutParkingTicketPane = new BorderLayout();
+		FlowLayout flowLayoutParkingDisplayPane = new FlowLayout(
+				FlowLayout.CENTER, 5, 10);
+		FlowLayout flowLayoutParkingCoinsButtonPane = new FlowLayout(
+				FlowLayout.CENTER, 5, 10);
+		FlowLayout flowLayoutParkingSlotPane = new FlowLayout();
+		FlowLayout flowLayoutParkingTicketfieldPane = new FlowLayout(
+				FlowLayout.CENTER, 5, 10);
 
-		int displayHeight = 25;
-		int numberFieldHeight = 60;
-		int currentX = 0;
-		int currentY = 0;
+		parkingMeterPane.setLayout(borderLayoutParkingMeterPane);
+		parkingBorderPane1.setLayout(flowLayoutParkingBorderPane1);
+		parkingMeterMMI.setLayout(flowLayoutParkingMeterMMI);
+		displayCoinsPane.setLayout(flowLayoutParkingDisplayCoinsPane);
+		buttonSlotPane.setLayout(flowLayoutParkingButtonSlotPane);
+		ticketPane.setLayout(borderLayoutParkingTicketPane);
+		displayPane.setLayout(flowLayoutParkingDisplayPane);
+		coinsButtonPane.setLayout(flowLayoutParkingCoinsButtonPane);
+		// coinsButtonPane.setLayout(new GridLayout(3, 1, 5, 5));
+		buttonPane.setLayout(new GridLayout(4, 1, 5, 5));
+		slotPane.setLayout(flowLayoutParkingSlotPane);
+		ticketfieldPane.setLayout(flowLayoutParkingTicketfieldPane);
 
-		// Draw background
-		g2.setColor(BG);
-		g2.fillRect((int) (currentX * factor), (int) (currentY * factor),
-				(int) (initialWidth * factor), (int) (initialHeight * factor));
+		parkingMeterPane.setPreferredSize(new Dimension(
+				(int) (initialWidth * factor), (int) (initialHeight * factor)));
+		parkingMeterMMI.setPreferredSize(new Dimension(
+				(int) ((initialWidth - 10) * factor),
+				(int) ((initialHeight - 150) * factor)));
+		feetPane.setPreferredSize(new Dimension((int) (initialWidth * factor),
+				(int) ((initialHeight - 180) * factor)));
+		displayCoinsPane.setPreferredSize(new Dimension(
+				(int) ((initialWidth - 10) * factor),
+				(int) ((initialHeight - 245) * factor)));
+		buttonSlotPane.setPreferredSize(new Dimension(
+				(int) ((initialWidth - 10) * factor),
+				(int) ((initialHeight - 240) * factor)));
+		ticketPane.setPreferredSize(new Dimension(
+				(int) ((initialWidth - 10) * factor),
+				(int) ((initialHeight - 250) * factor)));
+		displayPane.setPreferredSize(new Dimension(
+				(int) ((initialWidth - 50) * factor),
+				(int) ((initialHeight - 250) * factor)));
+		coinsButtonPane.setPreferredSize(new Dimension(
+				(int) ((initialWidth - 70) * factor),
+				(int) ((initialHeight - 250) * factor)));
+		buttonPane.setPreferredSize(new Dimension(
+				(int) ((initialWidth - 45) * factor),
+				(int) ((initialHeight - 250) * factor)));
+		slotPane.setPreferredSize(new Dimension(
+				(int) ((initialWidth - 75) * factor),
+				(int) ((initialHeight - 250) * factor)));
+		ticketfieldPane.setPreferredSize(new Dimension(
+				(int) ((initialWidth - 10) * factor),
+				(int) ((initialHeight - 250) * factor)));
 
-		// Draw inner background
-		currentX += 5;
-		currentY += 10;
+		this.setBackground(BG);
+		parkingMeterPane.setBackground(BG_INNER);
+		parkingMeterMMI.setBackground(BG_INNER);
+		feetPane.setBackground(BG);
+		displayCoinsPane.setBackground(BG_INNER);
+		buttonSlotPane.setBackground(BG_INNER);
+		ticketPane.setBackground(BG_INNER);
+		displayPane.setBackground(BG_DISPLAY);
+		coinsButtonPane.setBackground(BG_INNER);
+		buttonPane.setBackground(BG_INNER);
+		slotPane.setBackground(BG_INNER);
+		ticketfieldPane.setBackground(BG_INNER);
 
-		int innerPosX = currentX;
-		int innerWidth = initialWidth - 10;
-		int innerHeight = initialHeight - 60;
+		this.add(parkingMeterPane, BorderLayout.CENTER);
+		parkingMeterPane.add(feetPane, BorderLayout.SOUTH);
+		parkingMeterPane.add(parkingMeterMMI, BorderLayout.CENTER);
+		parkingMeterMMI.add(displayCoinsPane);
+		parkingMeterMMI.add(buttonSlotPane);
+		parkingMeterMMI.add(ticketPane);
 
-		g2.setColor(BG_INNER);
-		g2.fillRect((int) (currentX * factor), (int) (currentY * factor),
-				(int) ((innerWidth - 1) * factor),
-				(int) ((innerHeight - 1) * factor));
+		displayCoinsPane.add(displayPane);
+		displayCoinsPane.add(coinsButtonPane);
 
-		// Draw inner border / shadow
-		g2.setColor(Color.BLACK);
-		g2.draw3DRect((int) (currentX * factor), (int) (currentY * factor),
-				(int) ((innerWidth - 1) * factor),
-				(int) ((innerHeight - 1) * factor), true);
+		buttonSlotPane.add(buttonPane);
+		buttonSlotPane.add(slotPane);
 
-		// Draw display background
-		currentX += 20;
-		currentY += 15;
+		ticketPane.add(ticketfieldPane, BorderLayout.WEST);
 
-		int displayWidth = innerWidth - 40;
+		display = new JTextArea();
+		Dimension dimensionDisplay = new Dimension((int) (45 * factor),
+				(int) (20 * factor));
+		display.setPreferredSize(dimensionDisplay);
+		display.setEditable(false);
+		display.setBackground(BG_DISPLAY);
 
-		g2.setColor(BG_DISPLAY);
-		g2.fillRect((int) (currentX * factor), (int) (currentY * factor),
-				(int) ((displayWidth - 1) * factor),
-				(int) ((displayHeight - 1) * factor));
+		displayPane.add(display);
 
-		// Draw display border
-		g2.setColor(Color.BLACK);
-		g2.draw3DRect((int) (currentX * factor), (int) (currentY * factor),
-				(int) ((displayWidth - 1) * factor),
-				(int) ((displayHeight - 1) * factor), true);
+		coinsButtonPane.add(coin1);
+		coinsButtonPane.add(coin2);
+		coinsButtonPane.add(coin3);
+		coinsButtonPane.add(coin4);
 
-		// Draw display text
-		drawDisplayText(g2, currentX, currentY, currentX + displayWidth,
-				currentY + displayHeight);
+		buttonPane.add(button1);
+		buttonPane.add(button2);
+		buttonPane.add(button3);
+		buttonPane.add(button4);
+		buttonPane.add(button5);
+		buttonPane.add(button6);
+		buttonPane.add(button7);
+		buttonPane.add(button8);
+		buttonPane.add(button9);
+		buttonCancel.setBackground(BG_CANCEL);
+		buttonPane.add(buttonCancel);
+		buttonPane.add(button0);
+		buttonOk.setBackground(BG_OK);
+		buttonPane.add(buttonOk);
 
-		// Calculate marker triangle
-		int middle = initialWidth / 2;
-		currentY += displayHeight;
-		currentY += 3;
-		int infoTriangleWidth = displayWidth - 20;
+		coinSlut.setSize(10, 40);
+		coinSlut.setColumns(1);
+		coinSlut.setRows(4);
+		coinSlut.setEditable(false);
+		coinSlut.setBackground(BG_SLUT);
+		slotPane.add(coinSlut);
 
-		int tp1y = (int) (currentY * factor);
-		int tp2y = (int) ((currentY + (infoTriangleWidth / 3)) * factor);
-
-		int tp1x = (int) ((middle - (infoTriangleWidth / 2)) * factor);
-		int tp2x = (int) ((middle + (infoTriangleWidth / 2)) * factor);
-		int tp3x = (int) ((middle * factor));
-		currentY += (infoTriangleWidth / 3);
-
-		// Draw triangle background
-		g2.setColor(BG_MARKER);
-		g2.fillPolygon(new int[] { tp1x, tp2x, tp3x, tp1x }, new int[] { tp1y,
-				tp1y, tp2y, tp1y }, 4);
-
-		// Draw triangle border
-		g2.setColor(Color.black);
-		g2.drawPolyline(new int[] { tp1x, tp2x, tp3x, tp1x }, new int[] { tp1y,
-				tp1y, tp2y, tp1y }, 4);
-
-		// Draw number field background
-		currentY += 3;
-		g2.setColor(BG_NUMBER_FIELD);
-		g2.fillRoundRect((int) ((innerPosX + 20) * factor), (int) (currentY * factor),
-				(int) ((innerWidth - 41-1) * factor),
-				(int) ((numberFieldHeight-1) * factor), 10, 10);
-
-		// Draw number field border
-		g2.setColor(Color.BLACK);
-		g2.drawRoundRect((int) ((innerPosX + 20) * factor), (int) (currentY * factor),
-				(int) ((innerWidth - 41 - 1) * factor),
-				(int) ((numberFieldHeight - 1) * factor), 10, 10);
-		
-		// Draw Parking lot buttons
-		drawParkingLotButtons(g2,(innerPosX + 20),currentY,(innerPosX + innerWidth - 20),(currentY + numberFieldHeight));
-		
-		//Draw coin slot background
-		currentY += 3;
-			
-		//TODO: Draw Coin-Lot
-		
-		//TODO: Draw Buttons
-		
-		//Draw Booking-Button
-		
-		//Draw Cancel-Button
-
-		
-		//Draw Coin-Output and Ticket Output
-	}
-
-	/**
-	 * Draws the display text.
-	 * 
-	 * @param g
-	 *            The graphic object.
-	 * @param xFrom
-	 *            The x start position of the display.
-	 * @param xTo
-	 *            The x end position of the display.
-	 * @param yFrom
-	 *            The y start position of the display.
-	 * @param yTo
-	 *            The y end position of the display.
-	 */
-	private void drawDisplayText(Graphics2D g, int xFrom, int xTo, int yFrom,
-			int yTo) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	/**
-	 * Draws the parking-lot buttons
-	 * 
-	 * @param g
-	 *            The graphic object.
-	 * @param xFrom
-	 *            The x start position of the display.
-	 * @param xTo
-	 *            The x end position of the display.
-	 * @param yFrom
-	 *            The y start position of the display.
-	 * @param yTo
-	 *            The y end position of the display.
-	 */
-	private void drawParkingLotButtons(Graphics2D g, int xFrom, int xTo, int yFrom,
-			int yTo) {
-		// TODO Auto-generated method stub
+		ticketfield.setSize(10, 10);
+		ticketfield.setColumns(7);
+		ticketfield.setRows(4);
+		ticketfield.setEditable(false);
+		ticketfield.setBackground(BG_TICKETFIELD);
+		ticketfieldPane.add(ticketfield);
 
 	}
-
-
 }
