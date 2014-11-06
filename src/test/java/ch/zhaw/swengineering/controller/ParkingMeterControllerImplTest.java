27bb625068d832508742e3ec38c2e3c208ee8d56
Dani <daniel.brun@gmx.net>
2014-11-06 18:41:42 +0100

    Added Unit-Tests for booking calculation in slot machine

--- src/test/java/ch/zhaw/swengineering/controller/ParkingMeterControllerImplTest.java
+++ src/test/java/ch/zhaw/swengineering/controller/ParkingMeterControllerImplTest.java
@@ -1,11 +1,12 @@
 package ch.zhaw.swengineering.controller;
 
-import ch.zhaw.swengineering.helper.ConfigurationProvider;
-import ch.zhaw.swengineering.model.persistence.ParkingLot;
-import ch.zhaw.swengineering.model.persistence.ParkingMeter;
-import ch.zhaw.swengineering.model.persistence.SecretActionEnum;
-import ch.zhaw.swengineering.model.persistence.SecretCodes;
-import ch.zhaw.swengineering.setup.ParkingMeterRunner;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Hashtable;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -18,10 +19,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import java.util.Calendar;
-import java.util.Hashtable;
-
-import static org.mockito.Mockito.when;
+import ch.zhaw.swengineering.helper.ConfigurationProvider;
+import ch.zhaw.swengineering.model.ParkingLotBooking;
+import ch.zhaw.swengineering.model.persistence.ParkingLot;
+import ch.zhaw.swengineering.model.persistence.ParkingMeter;
+import ch.zhaw.swengineering.model.persistence.ParkingTimeDefinition;
+import ch.zhaw.swengineering.model.persistence.ParkingTimeDefinitions;
+import ch.zhaw.swengineering.model.persistence.SecretActionEnum;
+import ch.zhaw.swengineering.model.persistence.SecretCodes;
+import ch.zhaw.swengineering.setup.ParkingMeterRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ParkingMeterRunner.class, loader = AnnotationConfigContextLoader.class)
@@ -33,10 +39,15 @@ public class ParkingMeterControllerImplTest {
     @Mock(name = "secretCodesProvider")
     private ConfigurationProvider configProviderSecretCode;
 
+    @Mock(name = "parkingTimeDefinitionProvider")
+    private ConfigurationProvider configProviderTimeDef;
+
     @InjectMocks
     private ParkingMeterControllerImpl controller;
 
     private ParkingMeter parkingMeter;
+
+    private ParkingTimeDefinitions parkingTimeDefinitions;
 
     /**
      * Sets up a test case.
@@ -51,15 +62,20 @@ public class ParkingMeterControllerImplTest {
      */
     private void initStandard() {
         parkingMeter = getParkingMeterMock();
+        parkingTimeDefinitions = getParkingTimeDefinitionMock();
 
         when(configProviderParkingMeter.get()).thenReturn(parkingMeter);
+        when(configProviderTimeDef.get()).thenReturn(parkingTimeDefinitions);
 
         controller.init();
     }
 
     /**
-     * Argument: Invalid parking lot number. Method: 'getParkingLot' Expected
-     * result: Null
+     * Argument: Invalid parking lot number.
+     * 
+     * Method: 'getParkingLot'
+     * 
+     * Expected: Null
      */
     @Test
     public final void getParkingLotLotWithInvalidLotNumber() {
@@ -74,8 +90,11 @@ public class ParkingMeterControllerImplTest {
     }
 
     /**
-     * Argument: Valid parking lot number. Method: 'getParkingLot' Expected
-     * result: Correct parking lot object
+     * Argument: Valid parking lot number.
+     * 
+     * Method: 'getParkingLot'
+     * 
+     * Expected: Correct parking lot object
      */
     @Test
     public final void getParkingLotWithValidLotNumber() {
@@ -130,7 +149,8 @@ public class ParkingMeterControllerImplTest {
      * Expectation: The validation is not successful and an exception is thrown.
      */
     @Test(expected = Exception.class)
-    public final void exceptionShouldBeThrownWhenNoSecretActionsLoaded() throws Exception {
+    public final void exceptionShouldBeThrownWhenNoSecretActionsLoaded()
+            throws Exception {
 
         int secretKey = 999;
 
@@ -140,13 +160,14 @@ public class ParkingMeterControllerImplTest {
 
     /**
      * Method-Under-Test: validateSecretCodes(...).
-     *
+     * 
      * Scenario: The secret code mapping is invalid.
-     *
+     * 
      * Expectation: The validation is not successful and an exception is thrown.
      */
     @Test(expected = IllegalArgumentException.class)
-    public final void illegalArgumentexceptionShouldBeThrownWhenNoSecretActionCanBefound() throws Exception {
+    public final void illegalArgumentexceptionShouldBeThrownWhenNoSecretActionCanBefound()
+            throws Exception {
 
         int secretKey = 999;
         SecretActionEnum secretAction = SecretActionEnum.VIEW_ALL_INFORMATION;
@@ -164,6 +185,64 @@ public class ParkingMeterControllerImplTest {
 
         // Assert
         controller.getSecretAction(secretKey);
+    }
+
+    // ************** Tests booking calculation ***************
+
+    /**
+     * Argument: Valid data with enough money.
+     * 
+     * Method: 'calculateBookingForParkingLot'
+     * 
+     * Expected: All results are correct.
+     */
+    @Test
+    public final void testCalculateBookingForParkingLotWithEnoughMoney() {
+        // Setup
+        initStandard();
+
+        ParkingLotBooking booking = controller.calculateBookingForParkingLot(5,
+                new BigDecimal(5.0));
+
+        // 5.0 ->
+        Assert.assertNotNull(booking);
+        Assert.assertEquals(new BigDecimal(4).setScale(1),
+                booking.getChargedMoney());
+        Assert.assertEquals(new BigDecimal(2).setScale(1),
+                booking.getDrawbackMoney());
+        Assert.assertEquals(new BigDecimal(5), booking.getInsertedMoney());
+        Assert.assertEquals(booking.isNotEnoughMoney(), false);
+
+        int diff = (int) (booking.getPaidTill().getTime() - booking
+                .getPaidFrom().getTime());
+
+        int minutes = diff / 1000 / 60;
+
+        Assert.assertEquals(120, minutes);
+    }
+
+    /**
+     * Argument: Valid data with not enough money.
+     * 
+     * Method: 'calculateBookingForParkingLot'
+     * 
+     * Expected: All results are correct.
+     */
+    @Test
+    public final void testCalculateBookingForParkingLotWithNotEnoughMoney() {
+        // Setup
+        initStandard();
+
+        ParkingLotBooking booking = controller.calculateBookingForParkingLot(5,
+                new BigDecimal(0.5));
+
+        // 5.0 ->
+        Assert.assertNotNull(booking);
+        Assert.assertEquals(booking.isNotEnoughMoney(), true);
+        Assert.assertNull(booking.getChargedMoney());
+        Assert.assertEquals(booking.getDrawbackMoney(), new BigDecimal(0.5));
+        Assert.assertEquals(new BigDecimal(0.5), booking.getInsertedMoney());
+
     }
 
     /**
@@ -201,5 +280,25 @@ public class ParkingMeterControllerImplTest {
         parkingTime.set(Calendar.MINUTE, 20);
         parkingMeter.parkingLots.add(new ParkingLot(10, parkingTime.getTime()));
         return parkingMeter;
+    }
+
+    /**
+     * Creates a new parking time definition mock.
+     * 
+     * @return The mock.
+     */
+    private ParkingTimeDefinitions getParkingTimeDefinitionMock() {
+        ParkingTimeDefinitions definitions = new ParkingTimeDefinitions();
+
+        List<ParkingTimeDefinition> defList = new ArrayList<ParkingTimeDefinition>();
+
+        defList.add(new ParkingTimeDefinition(new BigDecimal(2.0), new Integer(
+                30), new Integer(2), 2));
+        defList.add(new ParkingTimeDefinition(new BigDecimal(1.0), new Integer(
+                30), new Integer(2), 1));
+
+        definitions.setParkingTimeDefinitions(defList);
+
+        return definitions;
     }
 }
